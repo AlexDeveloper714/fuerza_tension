6da7e366c8d9d03752260b3552ffa0ea5f175836
Alexander <idiazcruz@uniminuto.edu.co>
2017-05-17 12:24:45 -0500

    mejoras JS

--- js/aplicativo/sliderAcciones.js
+++ js/aplicativo/sliderAcciones.js
@@ -1,22 +1,3 @@
-//var Jquery
-var $document, selector, $inputRange;
-var value, value2, value3, value4;
-var $valor1, $valor2, $valor3, $valor4;
-var $tabla2, $tabla, $tabla3;
-
-//var Matter.js
-var Engine, engine, render;
-var verticesFi, triangulo, circle, circle_2, polea, group, cadena, cadena, verticesFl, flechas;
-var defaultCategory = 0x0001,
-        redCategory = 0x0002,
-        greenCategory = 0x0004,
-        blueCategory = 0x0008;
-var posXcir = 200, posYcir = 200, posXtria = 260, posYtria = 250, posXcadena = -80, posYcadena = 170;
-var x = posXcir, y = posYcir, difX = 0, difY = 0;
-//var calculos
-var angulo, masa1, masa2;
-var a = 200, b = 200, c = 0, tTotal = 0, tX = 0, tY = 0, w1 = 0, w2 = 0, gra = 9.8, peso1 = 0, peso2 = 0;
-var seno, coseno, radian;
 $(function () {
     //Inicializar Sliders :D
     $document = $(document);
